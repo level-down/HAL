20d43310fd69db4943cf4a0e1e4696f223b85957
hbj <heyhal19@gmail.com>
2020-06-24 15:32:35 -0700

    cleanup code

--- commands/rps.js
+++ commands/rps.js
@@ -1,7 +1,4 @@
 exports.run = (client, message, args) => {
-    console.log(client, message, args)
-    // message is a thing from discord.js, presumably `.reply` returns a promise, which has....something in it? looks like the message that you send in the reply, i suppose using this format allows you to catch connectivity errors
-    // args, by the time it gets here, is just the literal user input. so we should be able to split it by the @ symbol to find out which user the author is trying to rock paper scissors with
     let opponent = args.split('@')[1]
     let throw1 = rando()
     let throw2 = rando()
